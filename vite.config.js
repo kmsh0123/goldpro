5724558fde0fd12702450a7aac1db7184619534a
unknown <ingyinmay1932003@example.com>
2025-09-03 13:42:00 +0630

    main

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    'process.env.VITE_API_ENDPOINT': JSON.stringify(
-      process.env.NODE_ENV === 'production'
-        ? 'http://188.166.205.101:9002/api/'
-        : 'http://127.0.0.1:8000/api/'
-    ),
+  server: {
+    proxy: {
+      // Proxy API requests during development
+      "/api": {
+        target: "http://127.0.0.1:8000", // your local backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
   },
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -18,4 +21,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
